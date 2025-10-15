9a6d0d5b420385a2b2d57ea8748be1a4e944b6c4
ju5nj0se <juaan.josehernandez@gmail.com>
2025-10-14 21:06:10 -0500

    complete

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
+using HospitalSanVicente.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    try
+    {
+        context.Database.OpenConnection();
+        Console.WriteLine("Base de datos conectada ✅");
+        context.Database.CloseConnection();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("❌ Error de conexión a la base de datos:");
+        Console.WriteLine(ex.Message);          // Mensaje corto
+        Console.WriteLine(ex.StackTrace);       // Traza completa (útil para depuración)
+        if (ex.InnerException != null)
+            Console.WriteLine($"Detalle: {ex.InnerException.Message}");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
